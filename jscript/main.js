6ca6e647102c9a8b1c837663ec8382aa41205855
Student_XGA125 <Student_XGA125@macbook-air.home>
2020-07-09 10:55:44 -0400

    final edit to excercise

--- jscript/main.js
+++ jscript/main.js
@@ -4,27 +4,27 @@ const monthElem = document.getElementById('myMonth');
 /**excercise */
 let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday'];
 let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let daysLength = days.length - 1;
+let monthsLength = months.length - 1;
 let currentDay = 0;
 let currentMonth = 0;
 const showNewDay = n =>{
     currentDay += n;
-    if (currentDay > days.length - 1)
+    if (currentDay > daysLength)
         currentDay = 0;
     if (currentDay < 0)
-        currentDay = days.length;
-    console.log(days.length);
+        currentDay = daysLength;
     dayElem.innerHTML = days[currentDay];
 };
 const showNewMonth = n =>{
     currentMonth += n;
-    if (currentMonth > months.length)
+    if (currentMonth > monthsLength)
         currentMonth = 0;
     if (currentMonth < 0)
-        currentMonth = months.length;
-    console.log(days.length);
+        currentMonth = monthsLength;
     monthElem.innerHTML = months[currentMonth];
 };
 
 /**onload body effects */
-dayElem.innerHTML = showNewDay(0);
-monthElem.innerHTML = showNewMonth(0);
+dayElem.innerHTML = days[0];
+monthElem.innerHTML = months[0];
